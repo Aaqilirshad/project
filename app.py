7195589f34b6a8ab30db911ef725892a2c7ad338
root <root@DELL-Inspiron15.localdomain>
2022-05-23 12:20:32 +0530

    configure app

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-import cs50
-from flask import flask_session, Flask
+import os
+from cs50 import SQL
+from flask import flask_session, Flask, flash, redirect, render_template, request, session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+#configure application
+app = Flask(__name__)
+
+#make sure templates are auto reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True

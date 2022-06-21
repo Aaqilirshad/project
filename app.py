d2991197bb61a18b3e763644ae02566cbe3456a2
root <root@DELL-Inspiron15.localdomain>
2022-06-21 19:22:17 +0530

    Changes

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def login():
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
-        # Ensure username exists and password is correct
+        # Ensure username exists and password is correct.
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
            #return apology("invalid username and/or password", 403)
             flash("invalid username and/or password!")
